2615abc3097eaed6b2ac1549cee70cef45e128d8
장준영 <jangabc1@gmail.com>
2026-07-27 10:00:26 +0900

    feat: apply JPA auditing to clothing

--- backend/src/main/java/com/vesti/backend/entity/Clothing.java
+++ backend/src/main/java/com/vesti/backend/entity/Clothing.java
@@ -10,7 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Clothing {
+public class Clothing extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
